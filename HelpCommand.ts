b5f2c420aae6b05d018b3b1531ca30de06b2bb40
Alessio Palmero Aprosio <alessio@apnetwork.it>
2023-01-25 17:28:16 +0100

    Docker compatibility

--- HelpCommand.ts
+++ HelpCommand.ts
@@ -5,7 +5,7 @@ import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/def
 // https://github.com/RocketChat/Rocket.Chat/blob/a729aaa2baffea9338e1b0443cbac1780813bdd6/packages/rocketchat-emoji-emojione/emojiPicker.js
 
 export class HelpCommand implements ISlashCommand {
-    public static CommandName = 'aiuto';
+    public static CommandName = 'sos';
 
     public command: string = HelpCommand.CommandName;
     public i18nParamsExample: string = '';
@@ -16,23 +16,27 @@ export class HelpCommand implements ISlashCommand {
     public static sender: string = "kid_actions";
 
     public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp): Promise<void> {
-        const url = "http://dh-hetzner.fbk.eu:8001/test.php";
+        const url = "http://www/api/index.php";
         const message = context.getArguments().join(" ");
 
         const roomname = context.getRoom().displayName;
         if (roomname === undefined) {
-            await this.sendNotification("Error: unable to call /aiuto from direct message chat", ":warning:", modify, context, read);
+            await this.sendNotification("Error: unable to call /sos from direct message chat", ":warning:", modify, context, read);
             return;
         }
 
         const data = {
+            "action": "task",
+            "type": "rc",
+            "sub": "sos",
             "message": message,
             "roomid": context.getRoom().id,
             "roomname": roomname,
             "userid": context.getSender().id,
             "username": context.getSender().username,
         };
-        const response = await http.post(url, {query: "data=" + encodeURIComponent(JSON.stringify(data))});
+        // const response = await http.post(url, {query: "data=" + encodeURIComponent(JSON.stringify(data))});
+        const response = await http.post(url, {data: data});
 
         if (response === undefined) {
             await this.sendNotification("Error: unable to reach server", ":warning:", modify, context, read);
@@ -47,7 +51,8 @@ export class HelpCommand implements ISlashCommand {
             return;
         }
 
-        await this.sendNotification(response.content, ":sos:", modify, context, read);
+        let res = JSON.parse(response.content);
+        await this.sendNotification(res.message, res.avatar, modify, context, read);
     }
 
     public async sendNotification(text: string, icon: string, modify: IModify, context: SlashCommandContext, read: IRead): Promise<void> {
